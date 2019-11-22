b0da69edade9e7ec01f01e96fc29298b13be1cdc
Stephen Karl Jeoffrey  Hisole <skjghisole@yahoo.com>
2019-11-21 21:59:38 -0500

    added user model

---
+++ src/models/User.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+export type UserDocument = mongoose.Document & {
+    credentials: {
+        firstName: string;
+        lastName: string;
+    };
+
+    email: string;
+    password: string;
+};
+
+const userSchema = new mongoose.Schema({
+    credentials: {
+        firstName: String,
+        lastName: String
+    },
+    email: {
+        required: true,
+        unique: true,
+        type: String
+    },
+    password: {
+        require: true,
+        type: String
+    }
+}, { timestamps: {
+    createdAt: 'createdAt',
+    updatedAt: 'updatedAt'
+} });
+
+
+export const User = mongoose.model<UserDocument>("User", userSchema);
