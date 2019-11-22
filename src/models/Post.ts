fe111a7bb649f37b51c4cdfa71addbf63c437bbe
Stephen Karl Jeoffrey  Hisole <skjghisole@yahoo.com>
2019-11-21 21:59:24 -0500

    added post model

---
+++ src/models/Post.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+export type PostDocument = mongoose.Document & {
+    content: string;
+    senderId: string;
+};
+
+const postSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    senderId: {
+        type: String,
+        required: true
+    },
+}, { timestamps: {
+    createdAt: 'createdAt',
+    updatedAt: 'updatedAt'
+} });
+
+
+export const Post = mongoose.model<PostDocument>("Post", postSchema);
